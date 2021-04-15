538cb878c470154d5661f2b9e437479e648790fa
viniciuscsouza <viniciuscsouza@yahoo.com.br>
2021-04-15 16:29:52 -0300

    Chamada http

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,26 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/'
-}
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/viniciuscsouza/repos')
+            .then(response=>response.json())
+            .then(data=>setRepositories(data))
+    }, [repositories]);
+
     return(
       <section className="repository-list">
           <h1>Lista de repositórios</h1>
           <ul>
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
+              {repositories.map(repository=> {
+                return <RepositoryItem key={repository.name} repository={repository} />
+                }
+              )}
           </ul>
       </section>  
     );
